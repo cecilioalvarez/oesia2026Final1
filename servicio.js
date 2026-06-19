6fe503555734d634d1c82387b1e719895f2d1105
cecilio <cmac@arquitecturajava.com>
2026-06-19 11:40:42 +0200

    nuevo sumar

--- servicio.js
+++ servicio.js
@@ -2,7 +2,11 @@ class Servicio {
   imprimir() {
     console.log("Hola mas rapido");
   }
+    sumar( a,b) {
+    console.log(a+b);
+  }
 }
 console.log("******log de datos*")
 const servicio = new Servicio();
 servicio.imprimir();
+servicio.sumar(5, 10);
